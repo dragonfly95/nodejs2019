fe63a34625629527c6bf626ffaac6cab7d1990cd
dragonfly95 <nosignal95@gmail.com>
2020-02-09 23:33:46 +0900

    Add files via upload

--- Emily2/app.js
+++ Emily2/app.js
@@ -2,11 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
+const logger = require('./logger');
 
 var bodyParser = require('body-parser');
-
-
+const session = require('express-session');
+const bcrypt = require('bcrypt-nodejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,12 +18,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'ambc@!vsmkv#!&*!#EDNAnsv#!$()_*#@',
+  resave: false,
+  saveUninitialized: true
+}));
 
+app.use(morgan('combined', {stream: logger.stream}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,6 +43,89 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
+
+/**
+ * https://hyc7575.github.io/2017/06/02/2017-06-02-Nodejs-session-with-login/
+ * https://hyc7575.github.io/2017/06/02/2017-06-02-Nodejs-session-with-login2/
+ * https://hyc7575.github.io/2017/06/03/2017-06-03-NodeJs-session-with-login3/
+ */
+
+const users = [
+  {
+      user_id: 'admin',
+      user_nickname: '혁',
+      // user_pwd: '123456'
+      user_pwd: "$2a$10$Rq.T0yV3G3flg7prq5nPAuY32geCevDacaBD8ZNcbAnp9/On5i1pa"
+  },
+  {
+      user_id: 'root',
+      user_nickname: '에이치',
+      user_pwd: '1q2w3e'
+  }
+]
+
+
+const findUser = (user_id, user_pwd) => {
+  // id와 password가 일치하는 유저 찾는 함수, 없으면 undefined 반환
+  // return users.find( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
+  return users.find( v => (v.user_id === user_id && bcrypt.compareSync(v.user_pwd, user_pwd)) );
+}
+const findUserIndex = (user_id, user_pwd) => {
+  // 일치하는 유저의 index값(유니크) 반환
+  // return users.findIndex( v => (v.user_id === v.user_pwd === user_pwd) );
+  return users.findIndex( v => (v.user_id === user_id && bcrypt.compareSync(v.user_pwd, user_pwd)) );
+}
+
+
+app.get('/login', (req, res) => {
+  const sess = req.session;
+  if(sess.user_uid === undefined) {
+    res.render('login',{title:'login'});
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.post('/login', (req, res) => {
+  const body = req.body;
+
+
+  if(findUser( body.user_id, body.user_pwd )) {
+    req.session.user_uid = findUserIndex( body.user_id, body.user_pwd );
+     res.redirect('/');
+  } else {
+    res.end('유효하지 않습니다');
+  }
+});
+
+app.get('/logout', (req, res) => {
+  delete req.session.user_uid;
+   res.redirect('/');
+})
+//---------------------------------------------
+
+
+app.get('/join', (req, res) => {
+  res.render('join');
+});
+
+app.post('/join', (req, res) => {
+  const body = req.body;
+  if(body.user_id != 'admin') {
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(body.user_pwd, salt);
+
+    users.push({
+      user_id: body.user_id,
+      user_pwd: hash,
+      user_nickname: body.user_nickname
+    });
+    res.redirect('/login');
+  } else {
+    res.send('이미 존재함');
+  }
+})
 
 var multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
 var upload = multer({ dest: 'uploads/' })
@@ -79,6 +169,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  console.error(err.stack);
   res.render('error');
 });
 
--- Emily2/routes/index.js
+++ Emily2/routes/index.js
@@ -1,9 +1,104 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db");
+const winston = require('../logger');
+const { check, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/menus', (req, res) => {
+  const sess = req.session;
+  // Get Connection in Pool
+  makeQuery("select * from tbl_menu", null, (obj) => {
+    return res.json({data:obj});
+  });
+});
+
+router.get('/menus/:menuId', (req, res) => {
+  makeQuery('select * from tbl_menu where menu_id=?',[req.params.menuId], (obj) => {
+    winston.debug( req.params.menuId);
+    return res.json({data:obj[0]});
+  });
+});
+
+router.post('/menus', [
+  check('menu_price').isInt(),
+  check('cooking_time').isInt(),
+],(req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  
+  var sql = 'insert into tbl_menu (menu_name, menu_price, cooking_time) values (?,?,?)';
+  var param = [req.body.menu_name,req.body.menu_price,req.body.cooking_time];
+  db((err, connection) => {
+    connection.query(sql,param, (err, rows, fields) => {
+      connection.release();
+      if(err) throw err;
+      console.log(rows.insertId);
+      return res.json({ 'message': 'ok' });
+    })
+  });
+});
+
+router.put('/menus/:menuId', (req, res) => {
+  db((err, connection) => {
+
+    var param = [req.body.menu_name, req.body.menu_price,req.body.cooking_time, req.body.menu_id];
+    connection.query('update tbl_menu set menu_name=?, menu_price=?, cooking_time=? where menu_id=?',param, (err, rows,fields) => {
+      connection.release();
+      if(err) throw err;
+      return res.json({ data: rows[0]});
+    })
+  })
+});
+
+router.delete('/menus/:menuId', (req, res) => {
+  db((err, connection) => {
+
+    var param = [ req.params.menuId];
+    connection.query('delete from tbl_menu where menu_id=?',param, (err, rows,fields) => {
+      connection.release();
+      if(err) throw err;
+      return res.json({ data: rows[0]});
+    })
+  })
+});
+
+router.post('/bbs2', (req, res) => {
+  var sql = 'insert into tbl_menu set ?';
+  var param  = req.body;
+
+  db((err, connection) => {
+    connection.query(sql, param, (err, rows, fields) => {
+      connection.release();
+      if(err) {
+        return res.json({ 'message': 'error' });
+        throw err;
+      }
+      console.log(rows.insertId);
+      return res.json({ 'message': 'ok' });
+    })
+  });
+});
+
+
+
+function makeQuery(sql, param, callback) {
+  db((err,connection) => {
+    connection.query(sql, param, (err, rows) => {
+      connection.release(); // 연결세션 반환.
+      if (err) throw err;
+      callback(rows); // 결과는 rows에 담아 전송
+    });
+  });
+}
+
 
 module.exports = router;
